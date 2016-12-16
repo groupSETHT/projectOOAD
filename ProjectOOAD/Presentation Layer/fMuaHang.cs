913688b32997516e91180d283607f14a99f3426a
Group_SETHT <groupsetht@gmail.com>
2016-12-16 15:43:45 +0700

    fMuaHang.ver1

--- ProjectOOAD/Presentation Layer/fMuaHang.cs
+++ ProjectOOAD/Presentation Layer/fMuaHang.cs
@@ -9,19 +9,51 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using DevExpress.XtraEditors;
 
+using DTO;
+using BUS;
+
 namespace Presentation_Layer
 {
-    public partial class fMuaHang : DevExpress.XtraEditors.XtraForm
-    {
-        public fMuaHang()
+        public partial class fMuaHang : DevExpress.XtraEditors.XtraForm
         {
-            InitializeComponent();
+            NhaCungCapBUS objNhaCungCapBUS = new NhaCungCapBUS();
+            DataTable dtNhaCungCap;
+            public fMuaHang()
+            {
+                InitializeComponent();
+            }
+
+
+
+            public void LoadData()
+            {
+                try
+                {
+                    dtNhaCungCap = objNhaCungCapBUS.GetAllNhaCungCap();
+                    dataGridView1.DataSource = dtNhaCungCap;
+                    dataGridView1.RowHeadersVisible = false;
+                    dataGridView1.Columns["TenNhaCungCap"].HeaderText = "Nhà Cung Cấp";
+                    dataGridView1.Columns["MaNhaCungCap"].Visible = false;
+                    dataGridView1.Columns["SDT"].Visible = false;
+                    dataGridView1.Columns["DiaChi"].Visible = false;
+
+                }
+                catch
+                {
+                    MessageBox.Show("Kết nối dữ liệu thất bại!!!", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            private void fMuaHang_Load(object sender, EventArgs e)
+            {
+                try
+                {
+                    LoadData();
+                }
+                catch
+                {
+                    MessageBox.Show("Kết nối dữ liệu thất bại!!!", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+
         }
-
-        private void panelControl9_Paint(object sender, PaintEventArgs e)
-        {
-
-        }
-
     }
-}
